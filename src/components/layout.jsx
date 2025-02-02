54cd700659aacbddc8b0f1e715307cbd2c479331
ziyodyakubov <ziyodyakubov23@gmail.com>
2025-02-02 21:49:23 +0500

    finished

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -9,31 +9,31 @@ const Layout = () => {
     const navigate = useNavigate();
     return (
         <>
-            <main className="pr-[55px] pl-[35px] lg:pl-[0px] bg-[#fff]">
-                <div className="flex flex-col lg:flex-row lg:justify-between">
-                    <div className="pt-[70px] relative bottom-[500px] lg:top-[0px] flex flex-col items-start text-[#fff] pl-[76px] one-div">
-                        <h1 className="text-[51px] font-[600] leading-[62px] mb-[98px]">
-                            Create your <br /> account
+            <main className="pr-[55px] lg:pl-[0px] bg-[#fff]">
+                <div className="flex flex-col lg:flex-row justify-between">
+                    <div className="pt-[70px] items-center flex-col relative lg:left-[0px] lg:top-[0px] flex lg:flex-row lg:flex-col lg:items-start text-[#fff] lg:pl-[76px] one-div">
+                        <h1 className="lg:text-[51px] text-[35px] font-[600] leading-[62px] mb-[98px]">
+                            Create your <span className="inline lg:block">account</span>
                         </h1>
 
-                        <h3 className="text-[22px] mb-[80px]">
+                        <h3 className="text-[22px] hidden lg:block mb-[80px]">
                             Create an account using email.
                         </h3>
 
-                        <h2 className="text-[22px] font-[600] mb-[20px]">
+                        <h2 className="text-[17px] text-[#000] lg:text-start text-center lg:relative lg:text-[22px] absolute top-[400px] lg:top-[0px] lg:text-[#fff] font-[600] mb-[20px]">
                             Already have an IDP IELTS account?
                         </h2>
 
                         <button
                             onClick={() => navigate("/login")}
-                            className="w-[275px] h-[60px] font-[700] hover:text-[#fff] duration-150 transition-all hover:border-[1px] hover:bg-[#A2071F] hover:border-[#fff] bg-[#fff] rounded-[5px] text-[#A2071F] text-[20px]"
+                            className="w-[275px] h-[60px] absolute top-[450px] lg:top-[0px] text-[#FFf] bg-[#00AAAD] lg:relative font-[700] hover:text-[#fff] duration-150 transition-all hover:border-[1px] hover:bg-[#A2071F] hover:border-[#fff] lg:bg-[#fff] rounded-[5px] lg:text-[#A2071F] text-[20px]"
                         >
                             Log in
                         </button>
                     </div>
 
-                    <div className="lg:mt-[165px] relative top-[300px] lg:top-[0] mt-[0px] mr-[0px] lg:mr-[140px]">
-                        <h1 className="lg:text-[22px] text-[16px] font-[600] mb-[27px]">
+                    <div className="mt-[30px] pl-[35px] lg:mt-[165px] lg:mr-[140px] mr-[0px]">
+                        <h1 className="text-[18px] text-center lg:text-start lg:text-[22px] font-[600] mb-[27px]">
                             Sign-up using your email
                         </h1>
 
@@ -51,7 +51,7 @@ const Layout = () => {
                     </div>
 
                     <div>
-                        <img className="w-[220px] hidden lg:block mt-[15px]" src={IDP} alt="idp" />
+                        <img className="w-[220px] mt-[15px] hidden lg:block" src={IDP} alt="idp" />
                     </div>
                 </div>
 
--- src/pages/sign-in/index.jsx
+++ src/pages/sign-in/index.jsx
@@ -13,24 +13,24 @@ const Login = () => {
         if (formRef.current) {
             formRef.current.reportValidity();
             if (formRef.current.checkValidity()) {
-                navigate("/account")
+                navigate("/account");
             }
         }
     };
 
     return (
-        <main className='pr-[55px] bg-[#fff]'>
-            <div className='flex justify-between'>
-                <div className='pt-[75px] flex flex-col items-start text-[#fff] pl-[75px] one-div'>
+        <main className='pr-[55px] pb-[250px] bg-[#fff]'>
+            <div className='flex flex-col lg:flex-row justify-between'>
+                <div className='pt-[75px] flex flex-col items-start text-[#fff] pl-[50px] lg:pl-[75px] one-div'>
                     <img onClick={() => navigate("/")} src={BackButton} alt="BackButton" className='w-[23px] cursor-pointer relative bottom-[40px] right-[30px]' />
-                    <h1 className='text-[51px] font-[600] leading-[62px] mb-[100px]'>Log in to IDP <br /> IELTS</h1>
-                    <h3 className='text-[22px] mb-[80px] leading-[25px]'>Got an IDP IELTS account? Log in<br /> using email.</h3>
-                    <h2 className='text-[22px] font-[600] mb-[20px]'>Don’t have an account?</h2>
-                    <button onClick={() => navigate("/")} className='w-[275px] h-[60px] font-[700] hover:text-[#fff] duration-150 transition-all hover:border-[1px] hover:bg-[#A2071F] hover:border-[#fff] bg-[#fff] rounded-[5px] text-[#A2071F] text-[20px]'>Create an account</button>
+                    <h1 className='lg:text-[51px] text-[35px]  font-[600] leading-[62px] relative bottom-[30px] lg:flex lg:bottom-[0px] lg:mb-[100px]'>Log in to IDP <span className='inline lg:block-inline lg:relative lg:right-[290px] lg:top-[60px]'>IELTS</span></h1>
+                    <h3 className='text-[22px] mt-[0px] lg:mt-[60px] mb-[80px] hidden lg:block leading-[25px]'>Got an IDP IELTS account? Log in<br /> using email.</h3>
+                    <h2 className='lg:text-[22px] text-[19px] lg:left-[0px] left-[100px] text-center lg:text-start font-[600] lg:relative absolute text-[#000] lg:text-[#fff] lg:top-[0px] top-[650px] mb-[20px]'>Don’t have an account?</h2>
+                    <button onClick={() => navigate("/")} className='lg:w-[275px] text-[#fff] ml-[5px] lg:ml-[0px] bg-[#00AAAD] justify-center lg:mb-[100px] w-[280px] absolute lg:relative lg:top-[0px] top-[710px] h-[60px] font-[700] hover:text-[#fff] duration-150 transition-all hover:border-[1px] hover:bg-[#A2071F] hover:border-[#fff] lg:bg-[#fff] rounded-[5px] lg:text-[#A2071F] text-[20px]'>Create an account</button>
                 </div>
 
-                <div className='mt-[165px] mr-[140px]'>
-                    <h1 className='text-[22px] font-[600] mb-[35px]'>Log in with your email</h1>
+                <div className='mt-[40px] flex flex-col lg:block lg:flex-row items-center pl-[50px] lg:mt-[165px] mr-[0px] lg:mr-[140px]'>
+                    <h1 className='lg:text-[22px] font-[600] mb-[35px] text-[18px] text-center lg:text-start'>Log in with your email</h1>
 
                     <form ref={formRef} className='flex flex-col items-center mb-[20px]'>
                         <input type="email" required className='outline-none w-[315px] bg-[#FAFAFA] h-[60px] border-[#00000041] pl-[25px] rounded-t-[10px] border-t-[1px] border-l-[1px] border-r-[1px]' placeholder='yourname@gmail.com' />
@@ -43,7 +43,7 @@ const Login = () => {
                         </div>
                     </form>
 
-                    <span className='text-[13px] mb-[70px] text-[#656565] hover:text-[#656565d0] flex justify-end cursor-pointer'>Forgot your password?</span>
+                    <span className='text-[13px] lg:mb-[70px] mb-[50px] text-[#656565] hover:text-[#656565d0] flex relative lg:flex lg:left-[0px] left-[90px] justify-end cursor-pointer'>Forgot your password?</span>
 
                     <button type='button' onClick={handleSubmit} className='w-[270px] h-[60px] rounded-[2px] text-[21px] text-[#C0BFC0] hover:bg-[#05AAAD] hover:text-[#fff] transition-all duration-200 font-[700] bg-[#919191]'>
                         Log in
@@ -51,7 +51,7 @@ const Login = () => {
                 </div>
 
                 <div>
-                    <img className='w-[220px] mt-[15px]' src={IDP} alt="idp" />
+                    <img className='w-[220px] mt-[15px] hidden lg:block' src={IDP} alt="idp" />
                 </div>
             </div>
         </main>
